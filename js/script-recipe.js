4e1bad8d3a3108b8f87405b199b83bb2a1ed3d53
mthompsonc <m.paz.thompson@gmail.com>
2018-01-08 14:56:52 -0300

    Etapa 9

--- js/script-recipe.js
+++ js/script-recipe.js
@@ -1,17 +1,21 @@
-$(document).ready(function(){
-    // Eliminando Botón Menú.
-    $('.js-menu').remove();
-    //Etapa 7
-    console.log('Holi, está listo el documento.');
+$(document).ready(function() {
+  // Eliminando Botón Menú.
+  $('.js-menu').remove();
+  //Etapa 7
+  console.log('Holi, está listo el documento.');
 
 
-    $(".js-show-recipe").click(function() {
-   $("#pageRecipe").removeClass("make");
- });
+  $(".js-show-recipe").click(function() {
+    $("#pageRecipe").removeClass("make");
+    $(this).addClass('active');
+    $(".js-show-make").removeClass('active');
+  });
 
- $(".js-show-make").click(function() {
-   $("#pageRecipe").addClass("make");
- });
+  $(".js-show-make").click(function() {
+    $("#pageRecipe").addClass("make");
+    $(this).addClass('active');
+    $(".js-show-recipe").removeClass('active');
+  });
 
 
 });
