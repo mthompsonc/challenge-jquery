d54ae0364a369c2d0ad803787525305c62deb810
mthompsonc <m.paz.thompson@gmail.com>
2018-01-08 13:46:56 -0300

    Etapa 4 y 5 (perdon me olvidé)

--- js/script-index.js
+++ js/script-index.js
@@ -10,6 +10,8 @@ $(document).ready( function(){
 	//La variable "recipesArray" esta declarada en el archivo "data/recipes.js"
 	renderHighlightedRecipes(recipesArray);
 
+	renderActivities(activitiesArray);
+
 });
 
 
@@ -27,13 +29,15 @@ function renderHighlightedRecipes(recipesArray) {
 	for(var i = 0; i < recipesArray.length; i++){
 		//Condición si highlighted es verdadero
 		if (recipesArray[i].highlighted == true){
-			//
+			//Pasándole los datos que cumplieron el if.
 			renderRecipe(recipesArray[i]);
 		}
 	}
 
 	console.log('Recipes: ', recipesArray);
 }
+
+
 
 /*
 * Función que se encarga de pintar UNA recetas que tenga
@@ -43,6 +47,8 @@ function renderHighlightedRecipes(recipesArray) {
 */
 function renderRecipe(recipe) {
 	console.log('Voy a pintar la receta: ', recipe);
+
+		$('.list-recipes').append('<a class="item-recipe" href="#"> <span class="attribution"> <span class="title-recipe"> TITULO: ' + recipe.title + '</span> <span class="metadata-recipe"><span class="author-recipe"> AUTOR: ' + recipe.source.name + '</span> <span class="bookmarks-recipe"> <span class="icon-bookmark"></span></span> </span> </span> <img src=img/recipes/320x350/' + recipe.name + '.jpg /></a>');
 }
 
 
@@ -50,9 +56,31 @@ function renderRecipe(recipe) {
 /*
 * Función que se encarga de pintar todas las actividades
 */
+
+function renderActivities(activitiesArray) {
+    console.log('Activities: ', activitiesArray);
+
+    for(var i = 0; i < activitiesArray.length; i++){
+        renderActivity(activitiesArray[i]);
+    }
+        if(activitiesArray.length > 0){
+            $(".wrapper-message").hide();
+						console.log('holi9')
+        }
+    }
+
+/*
 function renderActivities(activitiesArray) {
 	console.log('Activities: ', activitiesArray);
-}
+	for (var i = 0; i < activitiesArray.length; i++){
+	}
+	//Condición si la longitud del array es diferente a 0
+	if (activitiesArray.length > 0){
+		//Pasándole los datos que cumplieron el if.
+		console.log('holi9');
+		$('.​wrapper-message​').hide();
+	}
+}*/
 
 /*
 * Función que se encarga de pintar una actividad
